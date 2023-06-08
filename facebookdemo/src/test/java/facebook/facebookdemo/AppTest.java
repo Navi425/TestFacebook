9d9b39edb2f36d3d743f5a9a7577a6667692c424
LENOVO <LENOVO@DESKTOP-87H58F7>
2023-06-08 11:45:29 -1200

    facebook testing

---
+++ facebookdemo/src/test/java/facebook/facebookdemo/AppTest.java
@@ -0,0 +1,20 @@
+package facebook.facebookdemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
